f4747de3069ae53987819db07705b19ed97adec6
Erodri6z <erikrodriguez2417@gmail.com>
2024-05-17 12:26:30 -0500

    add a landing page

---
+++ src/components/Landing/landing.jsx
@@ -0,0 +1,13 @@
+import './landing.css'
+
+function Landing() {
+  return (
+    <>
+    <div className="landing">
+      <h1 className='landing-title'>FRANZ HUERTA INC, LLC</h1>
+    </div>
+    </>
+  )
+}
+
+export default Landing
