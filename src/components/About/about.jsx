21fe97c466e14efa5fca3366d238c34dff46f4c2
Erodri6z <erikrodriguez2417@gmail.com>
2024-05-17 14:00:07 -0500

    add an about page

---
+++ src/components/About/about.jsx
@@ -0,0 +1,18 @@
+
+
+function About() {
+  return (
+    <>
+    <div className="about container">
+      <div className="about">
+        <h4></h4>
+        <p></p>
+      </div>
+      <span></span>
+      <div className="map-div">
+        <h4></h4>
+      </div>
+    </div>
+    </>
+  )
+}
