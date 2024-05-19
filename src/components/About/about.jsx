171dbfcaac54ed54b8cd2c1921f294868bd19f3b
Erodri6z <erikrodriguez2417@gmail.com>
2024-05-19 15:05:32 -0500

    finish the about component

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -14,7 +14,7 @@ function About() {
           <h3 className='about-title'> Where we operate</h3>
           <iframe 
           src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d261050.55328159034!2d-90.50881298975315!3d36.06639114573487!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1715973822123!5m2!1sen!2sus" 
-          width="300px" 
+          width="400px" 
           height="400px" 
           allowFullScreen="" 
           loading="lazy" 
