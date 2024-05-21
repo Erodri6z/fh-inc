ecca51396d04f3d1c44be2d1591474176027a041
Erodri6z <erikrodriguez2417@gmail.com>
2024-05-20 19:19:45 -0500

    stub up a contact page

---
+++ src/components/Comtact/contact.jsx
@@ -0,0 +1,19 @@
+function Contact () {
+  return (
+    <>
+    <div>
+      <div>
+        <div>
+          Call us
+          <button>English</button>
+          or
+          <button>Spanish</button>
+        </div>
+        <div></div>
+        {/* <div></div> */}
+      </div>
+    </div>
+    </>
+  )
+}
+
