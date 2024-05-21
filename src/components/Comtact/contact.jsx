cfe18be3443061ebc0bad4b94b9bcfe6f80df477
Erodri6z <erikrodriguez2417@gmail.com>
2024-05-20 22:35:32 -0500

    add an email form

--- src/components/Comtact/contact.jsx
+++ src/components/Comtact/contact.jsx
@@ -1,6 +1,26 @@
 import './contact.css'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function Contact () {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
+        publicKey: 'YOUR_PUBLIC_KEY',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      )
+  }
   return (
     <>
     <div className='contact-component'>
@@ -15,7 +35,15 @@ function Contact () {
           <button>Spanish</button>
         </div>
         <div className='email'>
-          
+        <form ref={form} onSubmit={sendEmail}>
+          <label className='form-name'>Name</label>
+          <input className="form-name-input"type="text" name="user_name" />
+          <label className='form-email'>Email</label>
+          <input className='form-email-input' type="email" name="user_email" />
+          <label className='form-message'>Message</label>
+          <textarea className='form-message-input' name="message" />
+          <input type="submit" value="Send" />
+        </form>
         </div>
         {/* <div></div> */}
       </div>
