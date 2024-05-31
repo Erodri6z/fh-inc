7aa4aebad6e4dd98519f739b25077b8919ac11bf
Erodri6z <erikrodriguez2417@gmail.com>
2024-05-30 19:35:11 -0500

    add a button to the landing

--- src/components/Landing/landing.jsx
+++ src/components/Landing/landing.jsx
@@ -1,5 +1,6 @@
 import './landing.css'
 import title from "./../../assets/icons/fhlogo.png"
+import { Link} from 'react-scroll'
 
 function Landing() {
 
@@ -8,6 +9,9 @@ function Landing() {
     <>
     <div className="landing" >
       <img className="franz" src={title} alt="Franz Huerta Inc, llc" />
+      <Link className='nav-item' to="contact" smooth={true} duration={500}> 
+      <button>Get a Quote Now!</button>
+      </Link>
     </div>
     </>
   )
